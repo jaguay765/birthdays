5042a4afd28527c9b410f2a8eca58868b59db062
jaguay765 <94021881+jaguay765@users.noreply.github.com>
2022-10-26 15:14:53 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,21 @@
+
+    export default [
+        {
+            id: 1,
+            name:'Josh',
+            age:'19 ', 
+            image:'June 2,2005'
+        },
+        { 
+            id: 1,
+            name:'Josh',
+            age:'19 ', 
+            image:'June 2,2005'
+        },
+        {
+            id: 1,
+            name:'Josh',
+            age:'19 ', 
+            image:'June 2,2005'
+        }    
+    ]
